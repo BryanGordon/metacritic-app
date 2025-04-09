fbc04e1056aa6741bf141c33607fa8a231e21af6
Bryan Gordon <bryanbge92@gmail.com>
2025-04-08 23:50:47 -0500

    Update main file

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+// import icon from './assets/icon.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Nuevo texto de ejemplo</Text>
-      <Text>Funciona perfecto</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Image 
+        source={{uri: 'https://www.metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg'}} 
+        style={{width: 215, height: 294}}
+      />
+      <Text style={{color:'white'}}>Open up App.js to start working on your app!</Text>
+      <Text style={{color:'white'}}>Nuevo texto de ejemplo</Text>
+      <Text style={{color:'white'}}>Funciona perfecto</Text>
+      <Button 
+        color={'grey'}
+        title='No tengo estilos' 
+        onPress={() => alert('Pulsacion')}
+      />
+
+      <TouchableHighlight
+        onPress={() => alert('Hola boton 2')}
+        underlayColor={'#09f'}
+        style={{marginTop:30, backgroundColor:'#333B2F', padding:15, borderRadius:7}}
+      >
+        <Text style={{color:'white'}}>Boton customizable</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -15,7 +33,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
