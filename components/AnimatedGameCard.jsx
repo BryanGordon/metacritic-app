bcfdfd9963aed13aebd5205df5204a85c828e7bd
Bryan Gordon <bryanbge92@gmail.com>
2025-04-10 19:25:51 -0500

    Create component

---
+++ components/AnimatedGameCard.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react'
+import { Animated } from 'react-native'
+import { Gamecard } from './GameCard'
+
+export function AnimatedGameCard ({game, index}) {
+  const opacity = useRef(new Animated.Value(0)).current
+
+  useEffect(() => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 450,
+      delay: index * 300,
+      useNativeDriver: true
+    }).start()
+  }, [opacity, index])
+
+  return (
+    <Animated.View style={{ opacity }}>
+      <Gamecard game={game} />
+    </Animated.View>
+  )
+}
