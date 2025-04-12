3a0f983ab77f62549be6f27f73bcb61e2da95480
Bryan Gordon <bryanbge92@gmail.com>
2025-04-11 20:29:07 -0500

    Create about page

---
+++ app/about.js
@@ -0,0 +1,35 @@
+import { Link } from 'expo-router'
+import { ScrollView, Text } from 'react-native'
+
+export default function About () {
+  return (
+    <ScrollView>
+      <Link href='/' className='text-purple-500 text-lg my-1'>Ir atras</Link>
+      <Text className='text-white font-bold mb-8 text-2xl'>Sobre el proyecto</Text>
+      <Text className='text-white text-base text-white/90 mb-4'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+      </Text>
+
+      <Text className='text-white text-base text-white/90 mb-4'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+      </Text>
+
+      <Text className='text-white text-base text-white/90 mb-4'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+      </Text>
+
+      <Text className='text-white text-base text-white/90 mb-4'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+      </Text>
+
+      <Text className='text-white text-base text-white/90 mb-4'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+      </Text>
+    </ScrollView>
+  )
+}
