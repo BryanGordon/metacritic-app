3432ee2b4c3e9933cab75d754a69b98f8ca35f72
Bryan Gordon <bryanbge92@gmail.com>
2025-04-14 20:05:36 -0500

    Format main code

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,17 +1,10 @@
 import { useEffect, useState } from 'react'
-import { Link } from 'expo-router';
-import { View, ActivityIndicator, FlatList, StyleSheet, Pressable } from 'react-native';
+import { View, ActivityIndicator, FlatList } from 'react-native'
 import { gamesData } from '../lib/metacritic'
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Logo } from './Logo'
 import { AnimatedGameCard } from './AnimatedGameCard'
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import { CircleInfoIcon } from '../components/Icons'
-
 
 export function Main () {
   const [games, setGames] = useState([])
-  const insets = useSafeAreaInsets()
 
   useEffect(() => {
     gamesData().then((games) => {
@@ -21,24 +14,17 @@ export function Main () {
 
   return (
     <View className='bg-black'>
-      
       {
         games.length === 0
           ? (<ActivityIndicator color='#8609E6' size='large' />)
           : (
-          <FlatList
-            data={games}
-            keyExtractor={game => game.id}
-            renderItem={({ item, index }) => <AnimatedGameCard game={item} index={index} /> }
-          />
+            <FlatList
+              data={games}
+              keyExtractor={game => game.id}
+              renderItem={({ item, index }) => <AnimatedGameCard game={item} index={index} />}
+            />
             )
       }
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  logoContainer: {
-    marginBottom: 20
-  }
-})
