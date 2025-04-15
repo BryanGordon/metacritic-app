9665e888c90ccc0f929fe843ab34b21c17464cae
Bryan Gordon <bryanbge92@gmail.com>
2025-04-15 18:51:15 -0500

    Format code

--- app/[id].js
+++ app/[id].js
@@ -1,9 +1,8 @@
-import { useLocalSearchParams, Link } from 'expo-router'
+import { useLocalSearchParams, Stack } from 'expo-router'
 import { ActivityIndicator, Image, Text, View } from 'react-native'
 import { Screen } from '../components/Screen'
-import { Stack } from 'expo-router'
 import { useEffect, useState } from 'react'
-import { gameDatabyID, gamesData } from '../lib/metacritic'
+import { gameDatabyID } from '../lib/metacritic'
 import { ScrollView } from 'react-native-web'
 
 export default function Detail () {
@@ -29,25 +28,27 @@ export default function Detail () {
       />
       <View>
         {
-          gameInfo === null ? (
-            <ActivityIndicator color={'purple'} size={'large'} />
-          ) : (
-            <ScrollView>
-              <View className='justify-center items-center text-center'>
-                <Image
-                  className='mb-4 rounded'
-                  source={{ uri: gameInfo.thumbnail }}
-                  style={{ width: 214, height: 294 }}
-                />
-                <Text className='text-white text-center font-bold text-2xl'>
-                  {gameInfo.title}
-                </Text>
-                <Text className='text-white/70 mt-4 text-left mb-8 text-base'>
-                  {gameInfo.short_description}
-                </Text>
-              </View>
-            </ScrollView>
-          )
+          gameInfo === null
+            ? (
+              <ActivityIndicator color='purple' size='large' />
+              )
+            : (
+              <ScrollView>
+                <View className='justify-center items-center text-center'>
+                  <Image
+                    className='mb-4 rounded'
+                    source={{ uri: gameInfo.thumbnail }}
+                    style={{ width: 214, height: 294 }}
+                  />
+                  <Text className='text-white text-center font-bold text-2xl'>
+                    {gameInfo.title}
+                  </Text>
+                  <Text className='text-white/70 mt-4 text-left mb-8 text-base'>
+                    {gameInfo.short_description}
+                  </Text>
+                </View>
+              </ScrollView>
+              )
         }
       </View>
     </Screen>
