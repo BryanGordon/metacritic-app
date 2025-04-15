4a1540777e5d24b2678d2d323cb5efe1be5335b4
Bryan Gordon <bryanbge92@gmail.com>
2025-04-14 19:49:54 -0500

    Update layout structure

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,27 @@
-import { Slot } from 'expo-router'
-import { View, Text } from 'react-native'
+import { Link, Stack } from 'expo-router'
+import { View, Text, Pressable } from 'react-native'
+import { Logo } from '../components/Logo'
+import { CircleInfoIcon } from '../components/Icons'
 
 export default function Layout () {
   return (
-    <View className='flex-1 bg-black items-center justify-center'>
-      <Slot />
+    <View className='flex-1'>
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: 'black' },
+          // headerShown: false
+          headerTintColor: 'white',
+          headerTitle: '',
+          headerLeft: () => <Logo />,
+          headerRight: () => (
+                  <Link asChild href='/about'>
+                    <Pressable className='text-purple-500 text-lg my-1'>
+                      <CircleInfoIcon />
+                    </Pressable>
+                  </Link>
+          )
+        }}
+      />
     </View>
   )
 }
