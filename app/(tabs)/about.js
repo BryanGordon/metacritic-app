031eeb253fc89cff5f87311f88a67ec3d18a413e
Bryan Gordon <bryanbge92@gmail.com>
2025-04-15 18:51:05 -0500

    Format code

--- app/(tabs)/about.js
+++ app/(tabs)/about.js
@@ -6,38 +6,38 @@ import { Screen } from '../../components/Screen'
 export default function About () {
   return (
     <Screen>
-    <ScrollView>
-      <Link asChild href='/'>
-        <Pressable className='my-1'>
-          {({ pressed }) => <HomeIcon style={{ opacity: pressed ? 0.5 : 1 }} />}
-        </Pressable>
-      </Link>
-      <Text className='text-white font-bold mb-8 text-2xl'>Sobre el proyecto</Text>
-      <Text className='text-white text-base text-white/90 mb-4'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
-      </Text>
+      <ScrollView>
+        <Link asChild href='/'>
+          <Pressable className='my-1'>
+            {({ pressed }) => <HomeIcon style={{ opacity: pressed ? 0.5 : 1 }} />}
+          </Pressable>
+        </Link>
+        <Text className='text-white font-bold mb-8 text-2xl'>Sobre el proyecto</Text>
+        <Text className='text-white text-base text-white/90 mb-4'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+        </Text>
 
-      <Text className='text-white text-base text-white/90 mb-4'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
-      </Text>
+        <Text className='text-white text-base text-white/90 mb-4'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+        </Text>
 
-      <Text className='text-white text-base text-white/90 mb-4'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
-      </Text>
+        <Text className='text-white text-base text-white/90 mb-4'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+        </Text>
 
-      <Text className='text-white text-base text-white/90 mb-4'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
-      </Text>
+        <Text className='text-white text-base text-white/90 mb-4'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+        </Text>
 
-      <Text className='text-white text-base text-white/90 mb-4'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
-      </Text>
-    </ScrollView>
+        <Text className='text-white text-base text-white/90 mb-4'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
+        </Text>
+      </ScrollView>
     </Screen>
   )
 }
