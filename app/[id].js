af64d4782e8fb2c5ede09f0aa27348ef724581d3
Bryan Gordon <bryanbge92@gmail.com>
2025-04-15 18:39:59 -0500

    Update dynamic page

--- app/[id].js
+++ app/[id].js
@@ -1,19 +1,55 @@
 import { useLocalSearchParams, Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import { ActivityIndicator, Image, Text, View } from 'react-native'
+import { Screen } from '../components/Screen'
+import { Stack } from 'expo-router'
+import { useEffect, useState } from 'react'
+import { gameDatabyID, gamesData } from '../lib/metacritic'
+import { ScrollView } from 'react-native-web'
 
 export default function Detail () {
   const { id } = useLocalSearchParams()
+  const [gameInfo, setGameInfo] = useState(null)
+
+  useEffect(() => {
+    if (id) {
+      gameDatabyID(id).then(setGameInfo)
+    }
+  }, [id])
 
   return (
-    <View className='flex-1 justify-center items-center bg-black'>
+    <Screen>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: '#ffee00' },
+          headerTintColor: 'black',
+          headerLeft: () => {},
+          headerRight: () => {},
+          headerTitle: 'Tarisland'
+        }}
+      />
       <View>
-        <Text className='text-white font-bold mb-8 text-2xl'>
-          Detalle del juego {id}
-        </Text>
-        <Link href='/' className='text-purple-500'>
-          Volver atras
-        </Link>
+        {
+          gameInfo === null ? (
+            <ActivityIndicator color={'purple'} size={'large'} />
+          ) : (
+            <ScrollView>
+              <View className='justify-center items-center text-center'>
+                <Image
+                  className='mb-4 rounded'
+                  source={{ uri: gameInfo.thumbnail }}
+                  style={{ width: 214, height: 294 }}
+                />
+                <Text className='text-white text-center font-bold text-2xl'>
+                  {gameInfo.title}
+                </Text>
+                <Text className='text-white/70 mt-4 text-left mb-8 text-base'>
+                  {gameInfo.short_description}
+                </Text>
+              </View>
+            </ScrollView>
+          )
+        }
       </View>
-    </View>
+    </Screen>
   )
 }
