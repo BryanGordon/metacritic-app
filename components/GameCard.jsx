5a0e120cd06b6aeb6c609557d8a289105c55e404
Bryan Gordon <bryanbge92@gmail.com>
2025-04-14 20:05:07 -0500

    Format component

--- components/GameCard.jsx
+++ components/GameCard.jsx
@@ -2,31 +2,31 @@ import { Link } from 'expo-router'
 import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
 import { styled } from 'nativewind'
 
-export function Gamecard ({game}) {
+export function Gamecard ({ game }) {
   const StyledPressable = styled(Pressable)
   return (
     <Link asChild href={`/${game.id}`}>
-   <StyledPressable className='active:opacity-60 border border-black active:border-white/50 mb-2 bg-gray-500/10 rounded-xl p-4'>
-   <View className='flex-row gap-4'>
-               <Image
-                 source={{ uri: game.thumbnail }}
-                 style={styles.imageCard}
-               />
-               <View className='flex-shrink'>
-               <Text style={styles.titleCard}>
-                 {game.title}
-               </Text>
-               <Text style={styles.descriptionCard}>
-                 {game.short_description.slice(0, 100)}...
-               </Text>
-   
-               <Text style={styles.genreCard}>
-                 {game.genre}
-               </Text>
-               </View>
-   </View>
-    </StyledPressable>
-   </Link>
+      <StyledPressable className='active:opacity-60 border border-black active:border-white/50 mb-2 bg-gray-500/10 rounded-xl p-4'>
+
+        <View className='flex-row gap-4'>
+          <Image
+            source={{ uri: game.thumbnail }}
+            style={styles.imageCard}
+          />
+          <View className='flex-shrink'>
+            <Text style={styles.titleCard}>
+              {game.title}
+            </Text>
+            <Text style={styles.descriptionCard}>
+              {game.short_description.slice(0, 100)}...
+            </Text>
+            <Text style={styles.genreCard}>
+              {game.genre}
+            </Text>
+          </View>
+        </View>
+      </StyledPressable>
+    </Link>
   )
 }
 
@@ -38,7 +38,7 @@ const styles = StyleSheet.create({
   imageCard: {
     width: 100,
     height: 147,
-    borderRadius: 10,
+    borderRadius: 10
   },
 
   titleCard: {
@@ -48,14 +48,14 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 4
   },
-  
+
   descriptionCard: {
     marginTop: 8,
     fontSize: 16,
     color: '#eee',
     flexShrink: 1
   },
-  
+
   genreCard: {
     fontSize: 18,
     fontWeight: 'bold',
