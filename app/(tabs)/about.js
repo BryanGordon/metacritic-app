4993030d246c2f3ba43b29d8cf6bcf9ae7a54497
Bryan Gordon <bryanbge92@gmail.com>
2025-04-15 18:39:43 -0500

    Move files to tabs folder
    - Moved index.js and about.js files

--- app/(tabs)/about.js
+++ app/(tabs)/about.js
@@ -1,10 +1,12 @@
 import { Link } from 'expo-router'
 import { Pressable, ScrollView, Text } from 'react-native'
-import { HomeIcon } from '../components/Icons'
+import { HomeIcon } from '../../components/Icons'
+import { Screen } from '../../components/Screen'
 
 export default function About () {
   return (
-    <ScrollView className='bg-black'>
+    <Screen>
+    <ScrollView>
       <Link asChild href='/'>
         <Pressable className='my-1'>
           {({ pressed }) => <HomeIcon style={{ opacity: pressed ? 0.5 : 1 }} />}
@@ -36,5 +38,6 @@ export default function About () {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
       </Text>
     </ScrollView>
+    </Screen>
   )
 }
