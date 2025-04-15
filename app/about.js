d1ecad937500f35dca9d3daddedd1d1602bc5a03
Bryan Gordon <bryanbge92@gmail.com>
2025-04-14 19:50:21 -0500

    Update about structure

--- app/about.js
+++ app/about.js
@@ -1,10 +1,16 @@
 import { Link } from 'expo-router'
-import { ScrollView, Text } from 'react-native'
+import { Pressable, ScrollView, Text } from 'react-native'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { HomeIcon } from '../components/Icons'
 
 export default function About () {
   return (
-    <ScrollView>
-      <Link href='/' className='text-purple-500 text-lg my-1'>Ir atras</Link>
+    <ScrollView className='bg-black'>
+      <Link asChild href='/'>
+        <Pressable className='my-1'>
+          {({ pressed }) => <HomeIcon style={{ opacity: pressed ? 0.5 : 1 }} />}
+        </Pressable>
+      </Link>
       <Text className='text-white font-bold mb-8 text-2xl'>Sobre el proyecto</Text>
       <Text className='text-white text-base text-white/90 mb-4'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?
